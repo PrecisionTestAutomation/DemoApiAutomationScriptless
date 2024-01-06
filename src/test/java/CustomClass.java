9e82d8e39a49cf8bab22fc0a659a450c941373fd
prabhat gaddam <gaddamprabhat@gmail.com>
2024-01-07 04:08:28 +0530

    Web Automation

--- src/test/java/CustomClass.java
+++ src/test/java/CustomClass.java
@@ -1,6 +1,22 @@
+package test.java;
+
+import main.java.com.precisiontestautomation.automation.utils.KeyInitializers;
+
 public class CustomClass {
 
     public String body(){
         return "PrecisionTestAutomation1";
+    }
+
+    public void LoadString(){
+        KeyInitializers.getDriver().get().get("https://www.amazon.in/");
+    }
+
+    public void fetchBody(){
+        System.out.println(KeyInitializers.getGlobalVariables().get().get("TestName1"));
+    }
+
+    public void getEmail(){
+        System.out.println(KeyInitializers.getGlobalVariables().get());
     }
 }
